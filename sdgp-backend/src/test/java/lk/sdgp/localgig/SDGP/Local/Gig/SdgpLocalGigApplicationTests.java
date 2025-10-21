4ec8baaad5e088d97cfb80834c177c77669d5b7c
daniya418 <101710651+daniya418@users.noreply.github.com>
2025-10-21 22:45:13 +0530

    added backend again

---
+++ sdgp-backend/src/test/java/lk/sdgp/localgig/SDGP/Local/Gig/SdgpLocalGigApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sdgp.localgig.SDGP.Local.Gig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdgpLocalGigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
