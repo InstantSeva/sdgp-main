115f051b98797c315794f57bfa700a54138006a8
daniya418 <101710651+daniya418@users.noreply.github.com>
2025-10-21 22:31:53 +0530

    Add files via upload

---
+++ SDGP-Backend/src/main/java/lk/sdgp/localgig/SDGP/Local/Gig/SdgpLocalGigApplication.java
@@ -0,0 +1,13 @@
+package lk.sdgp.localgig.SDGP.Local.Gig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdgpLocalGigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdgpLocalGigApplication.class, args);
+	}
+
+}
